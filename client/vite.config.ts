97f7555df9e6e926d220feddd9189e1be08e7243
Brendan Young <brendan.young@rovacloud.com>
2025-12-11 05:38:40 +1100

    installed shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  envDir: '..', // load .env from the monorepo root
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
